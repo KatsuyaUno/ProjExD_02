ba4d5c091f8d5c2b1a35a7123203dbb0965e4c0b
katsuya <c0a22023ef@edu.teu.ac.jp>
2023-10-03 15:02:03 +0900

    second commit

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,19 @@ import random
 
 WIDTH, HEIGHT = 800, 600
 
+def check_bound(obj_rct:pg.Rect):
+    """
+    引数:こうかとんRect or 爆弾rect
+    戻り値:タプル(横方向判定結果、縦方向判定結果)
+    画面内ならTrue,画面外ならFalse
+    """
+    yoko,tate=True,True
+    if obj_rct.left<0 or WIDTH<obj_rct.right:
+        yoko=False
+    if obj_rct.top<0 or HEIGHT<obj_rct.bottom:
+        tate=False
+    return tate,yoko
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -15,8 +28,25 @@ def main():
     bb = pg.Surface((20, 20))
     pg.draw.circle(bb, (255, 0, 0), (10, 10), 10)
     bb_rct=bb.get_rect()#surfaceからrect抽出
+    kk_rct=kk_img.get_rect()
+
+    kk_rct.center=(450,200)
     x=random.randint(0,WIDTH)
     y=random.randint(0,HEIGHT)
+    vx=+5
+    vy=+5
+    
+    delta={#ren3移動量リスト
+        pg.K_UP:(0,-5), 
+        pg.K_DOWN:(0,+5),
+        pg.K_LEFT:(-5,0),
+        pg.K_RIGHT:(+5,0),
+    }
+
+    
+    
+
+    
     bb_rct.center=(x,y)#rectにランダムな座標を設定する
     bb.set_colorkey((0, 0, 0))
 
@@ -28,11 +58,22 @@ def main():
                 return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [400, 200])
+        screen.blit(kk_img, kk_rct)#ren3移動量に応じて更新
+        bb_rct.move_ip(vx,vy)#ren2 爆弾を動かす
+
         screen.blit(bb, bb_rct)
+        key_lst=pg.key.get_pressed()#key有効化
+        sum_mv=[0,0]
+        
+        for key,mv in delta.items():#ren3key入力判断
+            if key_lst[key]:
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
+        kk_rct.move_ip(sum_mv)
+
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
